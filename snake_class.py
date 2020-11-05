ff28856eb3715f73e1c0a5e51b27208034fe96c5
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-05 11:43:27 +0000

    Update snake_class.py

---
+++ snake_class.py
@@ -0,0 +1,18 @@
+# creating snake class as child class of reptile
+from reptile_class import Reptile
+
+class Snake(Reptile):
+
+    def __init__(self):
+
+        super().__init__() # inheriting things from parent class
+
+        self.forked_tongue = True
+        self.venom = True
+        self.limbs = False
+
+    # creating functions for our snake class
+    def use_tongue_to_smell(self):
+        return 'I use tongue to taste'
+
+
