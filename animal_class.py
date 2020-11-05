ae6f343ac98cd43777d5181bafb53058d47f1e01
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-05 11:21:18 +0000

    created some classes

---
+++ animal_class.py
@@ -0,0 +1,26 @@
+# creating an animal class
+
+class Animal():
+
+    def __init__(self): # initialising the Animal class
+        self.alive = True
+        self.spine = True
+        self.lungs = True
+        self.eyes = True
+
+        # create behavrious to stay alive
+        def breathe(self):
+            return 'Keep breathing to stay alive'
+        
+        def move(self):
+            return 'move left, right, up and down'
+        
+        def procreate(self):
+            return 'find partner'
+        
+        def eat(self):
+            reurn 'nom nom nom' 
+
+# instantiate our class / create an object
+# cat = Animal()
+# cat as child has inherited everything from animaal
